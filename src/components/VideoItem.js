c53dea757f6f6aee84caf56c6045fe7506b37eee
github username <github email address>
2019-05-21 14:25:40 -0700

    create VideoItem.css for some custom styling

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './VideoItem.css';
 
 // Stateless functional component that will render a single video
 
 // const VideoItem = props => {
   const VideoItem = ({ video }) => {
   return (
-    <div className="item">
+    <div className="video-item item">
       <img className="ui image" src={video.snippet.thumbnails.medium.url} />
         <div className="content">
           <div className="header">
