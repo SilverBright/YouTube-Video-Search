9d2b784b964b0a67a98855a191cec144080523f5
github username <github email address>
2019-05-21 11:45:45 -0700

    create components folder and basic App.js file

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Class Component that will hold all of the different states for our application
+
+class App extends React.Component {
+  render() {
+    return <div>App</div>;
+  }
+}
+
+export default App;
+
