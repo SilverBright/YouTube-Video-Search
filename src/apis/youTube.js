5b829902657c3b5561bb83c31a872299a27737a1
github username <github email address>
2019-05-21 13:12:25 -0700

    create apis folder and youTube.js with API, install axios

---
+++ src/apis/youTube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyBBA1dEGrD2-RImE4VR2bJUMRWf1hiRts8';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});
+
