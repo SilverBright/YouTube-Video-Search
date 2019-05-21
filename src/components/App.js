52dee9513dbb6302e7f2d723b5901f91a404fce9
github username <github email address>
2019-05-21 12:30:16 -0700

    stub out basic App.js, add semantic-ui stylesheet to index.html

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 // Class Component that will hold all of the different states for our application
 
 class App extends React.Component {
   render() {
-    return <div>App</div>;
+    return (
+    <div>
+      <SearchBar />
+    </div>
+    );
   }
 }
 
