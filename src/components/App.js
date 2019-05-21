4fc12dbf08c2b84a72c5f37f5dbb27aa9bd9b23e
github username <github email address>
2019-05-21 13:38:10 -0700

    create state for videos in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,27 @@ import youTube from '../apis/youTube';
 // Class Component that will hold all of the different states for our application
 
 class App extends React.Component {
-  onSearchTermSubmit = (searchTerm) => {
-    youTube.get('/search', {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] };
+  }
+
+  onSearchTermSubmit = async (searchTerm) => {
+    const response = await youTube.get('/search', {
       params: {
         q: searchTerm
       }
     });
+
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
     <div className="ui container">
       <SearchBar onFormSubmit={this.onSearchTermSubmit} />
-
+      I have { this.state.videos.length } videos.
     </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,6 +7,7 @@ import React from 'react';
 class SearchBar extends React.Component {
   constructor(props) {
     super(props)
+    
     this.state = { searchTerm: ''};
   }
 
