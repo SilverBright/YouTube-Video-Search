09d77f982bb312cf2a36f2011829e82dc7cb79cb
github username <github email address>
2019-05-21 12:33:59 -0700

    add basic form for Searchbar search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-
+// 
 
 class SearchBar extends React.Component {
   render() {
     return (
-      <div>SearchBar</div>
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
     );
   }
 }
