c80e36fb7b0886a83ac2af9afe36e91b3dca1c7a
github username <github email address>
2019-05-21 17:10:23 -0700

    create VideoDetail.js to render video and title layout

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youTube from '../apis/youTube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 // Class Component that will hold all of the different states for our application
 
@@ -28,13 +29,17 @@ class App extends React.Component {
   // this callback will allow a child component (VideoList, VideoItem) to communicate BACK up to parent 
   // otherwise we just use props to communicate from parent to child in one direction
   onVideoSelect = (video) => {
-    console.log('from the App Component', video);
+    // console.log('from the App Component', video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
     <div className="ui container">
       <SearchBar onFormSubmit={this.onSearchTermSubmit} />
+      {/* passing down a single instance of a video */}
+      <VideoDetail video={this.state.selectedVideo} />
+      {/* passing down the video selection option and an array of videos */}
       <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
     </div>
     );
